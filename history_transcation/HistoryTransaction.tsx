51b6294ffc5946ac6b6f0b873b086e7a54fc73b9
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-13 02:47:46 +0800

    add paggination

--- history_transcation/HistoryTransaction.tsx
+++ history_transcation/HistoryTransaction.tsx
@@ -1,43 +1,49 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Card from "antd/lib/card";
-import { Avatar, List } from "antd";
+import { Avatar, List, Pagination } from "antd";
 import { IHistoryTransaction } from "./model/HistoryTranscationModel";
 
 export default function HistoryTransaction(props: IHistoryTransaction) {
   return (
-    <Card
-      bordered={false}
-      bodyStyle={{ paddingTop: 0 }}
-      className="header-solid h-full  ant-list-yes"
-      title={<h6 className="font-semibold m-0">Your Transactions</h6>}
-    >
-      {props.dataTranscation.map((item, index) => {
-        return (
-          <List
-            key={index}
-            header={<h6>{item.date}</h6>}
-            className="transactions-list ant-newest"
-            itemLayout="horizontal"
-            dataSource={item.data}
-            renderItem={(item) => (
-              <List.Item>
-                <List.Item.Meta
-                  avatar={
-                    <Avatar size="small" className={item.textclass}>
-                      {item.avatar}
-                    </Avatar>
-                  }
-                  title={item.title}
-                  description={item.description}
-                />
-                <div className="amount">
-                  <span className={item.amountcolor}>{item.amount}</span>
-                </div>
-              </List.Item>
-            )}
-          />
-        );
-      })}
-    </Card>
+    <Fragment>
+      <Card
+        style={{ marginBottom: 20 }}
+        bordered={false}
+        bodyStyle={{ paddingTop: 0 }}
+        className="header-solid h-full  ant-list-yes"
+        title={<h6 className="font-semibold m-0">Your Transactions</h6>}
+      >
+        {props.dataTranscation.map((item, index) => {
+          return (
+            <List
+              key={index}
+              header={<h6>{item.date}</h6>}
+              className="transactions-list ant-newest"
+              itemLayout="horizontal"
+              dataSource={item.data}
+              renderItem={(item) => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar size="small" className={item.textclass}>
+                        {item.avatar}
+                      </Avatar>
+                    }
+                    title={item.title}
+                    description={item.description}
+                  />
+                  <div className="amount">
+                    <span className={item.amountcolor}>{item.amount}</span>
+                  </div>
+                </List.Item>
+              )}
+            />
+          );
+        })}
+      </Card>
+      <div style={{display: 'flex', justifyContent: 'center', marginBottom: 20}}>
+        <Pagination defaultCurrent={1} total={50} />
+      </div>
+    </Fragment>
   );
 }
