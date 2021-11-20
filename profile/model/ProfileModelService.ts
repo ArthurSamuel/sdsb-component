d653c58b8a92b0f9d8727fdf5e22c728795528c5
Arthur Samuel <arthur.samuel@devstack.com.au>
2021-11-20 16:51:38 +0800

    add profile service

--- profile/model/ProfileModelService.ts
+++ profile/model/ProfileModelService.ts
@@ -1,35 +1,4 @@
-interface IMember {
-  id: number;
-  code: string;
-  parent_id: number;
-  type: string;
-  name: string;
-  address: string;
-  city: string;
-  zip_code: string;
-  province: string;
-  sub_district: string;
-  urban_village: string;
-  phone: string;
-  ktp: string;
-  npwp: string;
-  email: string;
-  username: string;
-  ktp_image: string;
-  profile_image: string;
-  bank_name: string;
-  bank_branch: string;
-  bank_acc: string;
-  description: string;
-  status: string;
-  created_by: number;
-  updated_by: number;
-  deleted_by: number;
-  deleted_at: string;
-  created_at: string;
-  updated_at: string;
-}
-
+import { IMember } from "../../models/Member";
 interface IDataProfile {
   id: number;
   username: string;
