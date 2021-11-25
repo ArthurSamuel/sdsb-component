53dd2153c0560ff7580901dbd296d4a4ffa2fd1d
Arthur Samuel <arthursamuel1696@gmail.com>
2021-11-25 19:06:03 +0800

    add margin to spinner

--- spinner/Spinner.tsx
+++ spinner/Spinner.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Spin } from "antd";
 
-export default function Spinner() {
+interface ISpinner {
+  marginTop?: number;
+}
+
+export default function Spinner(props: ISpinner) {
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100%",
+        marginTop: props.marginTop,
+      }}>
       <Spin></Spin>
     </div>
   );
