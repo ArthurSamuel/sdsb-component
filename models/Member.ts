2330b3bf399445a311a73ea9364e70cd616a8c4d
Arthur Samuel <arthursamuel1696@gmail.com>
2021-11-25 19:05:22 +0800

    add profile model

--- models/Member.ts
+++ models/Member.ts
@@ -29,3 +29,28 @@ export interface IMember {
   created_at: string;
   updated_at: string;
 }
+
+export interface IProfileModel {
+  id: number;
+  username: string;
+  type: string;
+  member_id: number;
+  customer_id: number;
+  name: string;
+  email: string;
+  image: string;
+  description: string;
+  status: string;
+  pin: string;
+  member: IMember;
+  created_by: number;
+  updated_by: number;
+  deleted_by: number;
+  deleted_at: null;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface IProfile {
+  data: IProfileModel
+}
